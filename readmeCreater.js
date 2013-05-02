92f77c40c36ff6024a310ddc4a81e5f4f8ff43fe
insung <yooninsung@gmail.com>
2013-05-02 10:08:18 +0900

    Chapter 6 Commit
    without images and other resources .. !

---
+++ readmeCreater.js
@@ -0,0 +1,19 @@
+﻿var fs = require('fs');
+var ejs = require('ejs');
+
+var template = fs.readFileSync('ReadmeEJS.html', 'utf8')
+console.log(template)
+for (var i = 1; i < 12; i++) {
+    (function (i) {
+        fs.readdir('chapter_' + i, function (error, result) {
+            if (error) {
+                console.log('There is no Directory');
+            } else {
+                fs.writeFile('Chapter ' + i + '.html', ejs.render(template, {
+                    title: 'Chapter_' + i,
+                    files: result
+                }));
+            }
+        });
+    })(i);
+}
