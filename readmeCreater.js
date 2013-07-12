e34a64ba0001406b19cb6265c45377f32426e191
insung <yooninsung@gmail.com>
2013-07-12 12:16:07 +0900

    else

--- readmeCreater.js
+++ readmeCreater.js
@@ -4,16 +4,19 @@ var ejs = require('ejs');
 var template = fs.readFileSync('ReadmeEJS.html', 'utf8');
 var jsTemplate = fs.readFileSync('CodeEJS.html', 'utf8');
 
-for (var i = 1; i < 12; i++) {
+for (var i = 1; i <= 12; i++) {
     (function (i) {
         fs.readdir('chapter_' + i, function (error, result) {
             if (error) {
                 console.log('There is no Directory');
             } else {
-                if (i > 8) {
+                if (i >= 8) {
                     var texts = {};
                     for (var j = 0; j < result.length; j++) {
-                        texts[result[j]] = fs.readFileSync('chapter_' + i + '/' + result[j], 'utf8');
+                        try{
+                            texts[result[j]] = fs.readFileSync('chapter_' + i + '/' + result[j], 'utf8');
+                        } catch (e) { }
+                            
                     }
 
                     fs.writeFile('Chapter ' + i + '.html', ejs.render(jsTemplate, {
